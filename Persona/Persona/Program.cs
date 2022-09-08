423279ea1d1f03120b340fb0e545b2aaba9652a5
dariodgts <dariogregoriotejeda@gmail.com>
2022-09-08 03:36:46 -0400

    este es un ejemplo de una clase persona en lenguaje c# con programacion orientada a objectos

---
+++ Persona/Persona/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Persona
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        { 
+string nombre;
+        byte edad;
+        int telefono;
+            Console.WriteLine("CUAL ES SU NOMBRE");
+            nombre = Console.ReadLine();
+            Console.WriteLine("digite su telefono");
+            telefono = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("digite su edad");
+            edad = Convert.ToByte(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine("nombre:" + nombre);
+            Console.WriteLine("telefono:" + telefono);
+            Console.WriteLine("edad:" + edad);
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
